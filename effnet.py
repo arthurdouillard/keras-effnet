86353921c8b4e0664235b34294a09c7e5c59daee
Arthur Douillard <douill_a@epita.fr>
2018-05-06 18:47:25 +0200

    Add effnet.

---
+++ effnet.py
@@ -0,0 +1,55 @@
+from keras.models import Model
+from keras.layers import *
+from keras.activations import *
+from keras.callbacks import *
+
+
+def get_post(x_in):
+    x = LeakyReLU()(x_in)
+    x = BatchNormalization()(x)
+    return x
+
+def get_block(x_in, ch_in, ch_out):
+    x = Conv2D(ch_in,
+               kernel_size=(1, 1),
+               padding='same',
+               use_bias=False)(x_in)
+    x = get_post(x)
+
+    x = DepthwiseConv2D(kernel_size=(1, 3), padding='same', use_bias=False)(x)
+    x = get_post(x)
+    x = MaxPool2D(pool_size=(2, 1),
+                  strides=(2, 1))(x) # Separable pooling
+
+    x = DepthwiseConv2D(kernel_size=(3, 1),
+                        padding='same',
+                        use_bias=False)(x)
+    x = get_post(x)
+
+    x = Conv2D(ch_out,
+               kernel_size=(2, 1),
+               strides=(1, 2),
+               padding='same',
+               use_bias=False)(x)
+    x = get_post(x)
+
+    return x
+
+
+def Effnet(input_shape, nb_classes, include_top=True, weights=None):
+    x_in = Input(shape=(32,32,3))
+
+    x = get_block(x_in, 32, 64)
+    x = get_block(x, 64, 128)
+    x = get_block(x, 128, 256)
+
+    if include_top:
+        x = Flatten()(x)
+        x = Dense(nb_classes, activation='softmax')(x)
+
+    model = Model(inputs=x_in, outputs=x)
+
+    if weights is not None:
+        model.load_weights(weights, by_name=True)
+
+    return model
